fda57ca80153fa5effdaea6b116f71f5fe856f02
Falkner Dominik <falkner.dominik@gmail.com>
2016-10-27 12:53:05 +0200

    Add various config files
        * xfce4
        * pdfeditor
        * thunar
        * dconf
        * gconf
        * mimeapps

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -60,6 +60,7 @@
 ; (gtk_accel_path "<Actions>/ThunarBookmarks/da964a544b4ea040f67bdf8230e8df20" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/properties" "<Alt>Return")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/delete" "")
+; (gtk_accel_path "<Actions>/ThunarActions/Twp::setwallpaper" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/empty-trash" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-menu" "")
 ; (gtk_accel_path "<Actions>/ThunarLauncher/open-in-new-window" "<Primary><Shift>o")
