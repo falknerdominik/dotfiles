8a9a5c5178ce7fb4a859121d353b67dc0968a90b
Falkner Dominik <falkner.dominik@gmail.com>
2017-02-16 20:45:42 +0100

    Remove useless files, Updated some config files
        * Keymaps to i3 (+ script)
        * Wallpaper
        * albert config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ alias ... "cd ..; cd .."
 alias l. "ls -al"
 
 alias fixsteam 'find ~/.steam/root/ \( -name "libgcc_s.so*" -o -name "libstdc++.so*" -o -name "libxcb.so*" -o -name "libgpg-error.so*" \) -print -delete'
+alias gdqlive 'livestreamer --twitch-oauth-token bhlrfatmoujyxwvfsqi8d60vzb461f twitch.tv/gamesdonequick high'
 
 alias cdstud="cd ~/Nextcloud/school/Hagenberg/Studium/2016/WS2016"
 
